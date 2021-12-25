605c72f30bc7015d3e8a12e64d57b4c3d2037d74
Seishu Murayama <murayama.seishu@gmail.com>
2021-12-25 23:31:45 +0900

    initial

---
+++ rest-server/src/rest_server/main.clj
@@ -0,0 +1,13 @@
+(ns rest-server.main
+  (:gen-class)
+  (:require [duct.core :as duct]))
+
+(duct/load-hierarchy)
+
+(defn -main [& args]
+  (let [keys     (or (duct/parse-keys args) [:duct/daemon])
+        profiles [:duct.profile/prod]]
+    (-> (duct/resource "rest_server/config.edn")
+        (duct/read-config)
+        (duct/exec-config profiles keys))
+    (System/exit 0)))
